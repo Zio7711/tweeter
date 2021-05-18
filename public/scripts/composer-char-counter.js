a59c04885993154da5f6e43c8e9ef2f0e4b939bf
Zio <zi.ao.tan@outlook.com>
2021-05-18 22:06:23 +0000

    Add some edge cases for calculating the remaining chars

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,8 +4,27 @@ $(document).ready(() => {
 
   //register an event handler to the textarea element for the form.
   $('#tweet-text').on('keyup', function() {
+    //extract the input value and calculate its length
     const maxChar = 140;
     let typedInput = this.value;
-    console.log(this);
+    const alphabet =
+      'abcdefghijklmnopqrstuvwxyz' + 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
+    const alphabetArray = alphabet.split('');
+
+    //if the input is in the alphabet, it counts as 2 chars, else it count as 1
+    let inputLength = 0;
+    for (const typedChar of typedInput) {
+      if (alphabetArray.includes(typedChar)) {
+        inputLength += 2;
+      } else {
+        inputLength++;
+      }
+    }
+
+    //calculate the remaining chars for input field
+    let charRemaining = maxChar - inputLength;
+
+    //traversing up the DOM tree
+    $(this).parent().find('.counter').text(charRemaining);
   });
 });
