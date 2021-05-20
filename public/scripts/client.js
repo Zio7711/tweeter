f10b400617d2bec14b26e6abd452e554e81f9409
Zio <zi.ao.tan@outlook.com>
2021-05-20 02:47:38 +0000

    create an escape funtion to avoid xss

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -40,10 +40,18 @@ $(document).ready(() => {
     //calculate the time passed since tweeted
     let time = timeago.format(tweetObj['created_at']);
 
+
+    //create an escape funtion to avoid xss
+    const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
+
     //create child element content for article
     const $content = $(`
     <div>
-      <h2>${tweetObj.content.text}</h2>
+      <h2 class="contentBreak">${escape(tweetObj.content.text)}</h2>
       <div class="tweetsFooter">
         <span>${time}</span>
         <div>
@@ -86,37 +94,26 @@ $(document).ready(() => {
     const remainingNum = $('.counter')[0].value;
     if (remainingNum < 0) {
       $('.errorMsg')
-        .text('Your input is too LONG!!!')
-        .show()
-        .delay(2500)
-        .fadeOut();
-    } else if (remainingNum === '140' || this[0].value) {
+        .text('TOO Long!!! make it concise!!!')
+        .slideDown(1000)
+        .delay(2000)
+        .fadeOut(1)
+    } else if (remainingNum === '140' || !this[0].value.split("\n").join("")) {
       $('.errorMsg')
-        .text('Please input something!!!!')
-        .show()
-        .delay(2500)
-        .fadeOut();
+        .text('Yo!!! Type somthing!!!')
+        .slideDown(1000)
+        .delay(2000)
+        .fadeOut(1)
     } else {
       let formData = $(this).serialize();
       $.ajax('/tweets', { method: 'POST', data: formData })
         .then(() => {
           $('textarea').val('');
+          $('.counter').text(140);
           loadTweets();
         })
         .catch((error) => console.log(error));
     }
   });
 
-  //Validate function to check if the input info is ok
-  const inputMessage = (remaining) => {
-    console.log($('.counter').value);
-
-    let errorMessage = '';
-    if (content.length > 140) {
-      errorMessage = 'Your message is too long!!!!!';
-    } else if (content === '') {
-      errorMessage = "Don't leave it blank!!!";
-    }
-    return errorMessage;
-  };
 });
